f71e7eb4d96f0f6f8aa6e53f3bf59b786640be50
OlivierDM1999 <181964@umons.ac.be>
2022-04-16 09:41:09 +0200

    jeu javascript

--- popit_django/popit_app/urls.py
+++ popit_django/popit_app/urls.py
@@ -4,5 +4,7 @@ from popit_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('game',views.game,name="game"),
+    path('video_stream', views.video_stream, name='video_stream'),
     #path('', views.hello_world, name='hello_world'),
 ]
--- popit_django/popit_app/views.py
+++ popit_django/popit_app/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http.response import StreamingHttpResponse
+import cv2
+import numpy as np
+
+
+
+class VideoCamera(object):
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+    def __del__(self):
+        self.cap.release()
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        frame_flip = cv2.flip(frame, 1)
+        ret, frame = cv2.imencode('.jpg', frame_flip)
+
+        return frame.tobytes()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def video_stream(request):
+    video_object=gen(VideoCamera())
+    return StreamingHttpResponse(video_object,
+                    content_type='multipart/x-mixed-replace; boundary=frame')
+
+
 
 def index(request):
     return render(request,"hello.html")
+
+
+def game(request):
+    return render(request,"game.html")
+
+
 
